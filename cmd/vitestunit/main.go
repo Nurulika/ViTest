228de0af65704d33c923edb3a604b529de49d9e5
Nuru <ceruks@eteb.me>
2025-10-03 21:17:29 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/vitestunit/main.go
@@ -0,0 +1,23 @@
+// cmd/vitestunit/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "vitestunit/internal/vitestunit"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := vitestunit.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
